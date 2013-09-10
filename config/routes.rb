f8e7074ab583d406e8a6ca993cee2cb32f3efcc2
Nick Horob <nickhorob@gmail.com>
2013-09-09 22:38:12 -0500

    remove end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Omrails::Application.routes.draw do
   match '/', to: redirect('/foo.html')
-end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
